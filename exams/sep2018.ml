e8a2121686449bf04734cca21868b90bd98aebff
Matteo Galdi <git@matteogaldi.dev>
2021-09-10 12:27:07 +0200

    Add new exercise

--- exams/sep2018.ml
+++ exams/sep2018.ml
@@ -23,6 +23,30 @@ let sorted_branch t x =
 				else raise NotFound
 	in aux [] t
 
+type 'a graph = ('a * 'a) list
+let rec successori nodo = function
+    [] -> []
+  | (x,y)::rest -> 
+      if x=nodo then y::successori nodo rest
+      else successori nodo rest
+
+let _succ nodo grafo = 
+  List.map snd (List.filter (fun (x,y) -> x=nodo) grafo)
+
+
+let sorted_path g start goal = 
+  let rec from_node visited path n = 
+    if List.mem n visited then raise NotFound
+    else if n=goal && is_sorted (path@[n]) then [n]
+    else n::from_list (n::visited) (path@[n]) (successori n g)
+  and from_list visited path = function
+      [] -> raise NotFound
+    | n::rest -> 
+        try from_node visited path n
+        with NotFound -> from_list visited path rest
+  in from_node [] [] start
+
+let grafo = [(1,2); (2,3); (3,2); (3,5); (5,1); (5,6); (6,4)];;
 (*
 #use "sep2018.ml";;
 *)
