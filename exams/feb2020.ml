626699b0d03341e7849b6d0c9c513f1116c4c92f
Matteo Galdi <git@matteogaldi.dev>
2021-09-12 18:27:24 +0200

    Add "raggiungi" exercise

--- exams/feb2020.ml
+++ exams/feb2020.ml
@@ -1,4 +1,5 @@
 type metro = (int * int * string) list
+exception NotFound
 
 let m = [(1,2,"A"); (2,3,"A"); (3,1,"A"); (2,4,"B"); (4,5,"B"); (4,6,"C"); (6,3,"C"); (5,7,"D"); (6,7,"D")];;
 
@@ -28,6 +29,21 @@ let vicini stazione m =
 		if stazione = s1 then (s2, l) :: aux rest
 		else (s1, l) :: aux rest
 	in aux (List.filter (fun (s1, s2, _) -> s1=stazione || s2=stazione) m)
+
+
+let raggiungi (m: metro) maxc start goal = 
+	let rec from_node visited linea cambi s = 
+		if List.mem s visited || cambi>maxc then raise NotFound
+		else if s=goal then [s]
+		else s::from_list (s::visited) linea cambi (vicini s m)
+	and from_list visited linea cambi = function
+		| [] -> raise NotFound
+		| (s, l)::rest -> 
+			let new_cambi = if l=linea then cambi else cambi+1
+			in
+				try from_node visited l new_cambi s
+				with NotFound -> from_list visited l new_cambi rest
+	in from_node [] "" (-1) start
 
 (*
 #use "feb2020.ml";;
